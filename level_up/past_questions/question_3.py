e5399b22c80eed18680f4db6a3f50419eb32fac6
maresaku <78681238+maresaku@users.noreply.github.com>
2021-06-20 16:22:52 +0900

    Create question_3.py

---
+++ level_up/past_questions/question_3.py
@@ -0,0 +1,18 @@
+宝くじ
+#https://paiza.jp/works/mondai/skillcheck_archive/lottery
+
+b  = int(input())
+n  = int(input())
+
+for a in range(n):
+    a=int(input())
+    if a==b:
+        print("first")
+    elif a==b-1 or a==b+1:
+        print("adjacent")
+    elif a%10000 == b%10000:
+        print("second")
+    elif a%1000 == b%1000:
+        print("third")
+    else:
+        print("blank")
